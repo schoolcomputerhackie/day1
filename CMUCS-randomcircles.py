da140938689cefaa3fd81264453696df4142f596
don't skid my projects <144237855+anthonyseyler@users.noreply.github.com>
2023-09-06 13:40:15 -0400

    Create CMUCS-randomcircles.py

---
+++ CMUCS-randomcircles.py
@@ -0,0 +1,5 @@
+# made by willow
+from random import randint
+while True:
+    sleep(0.005)
+    Circle(randint(1, 400), randint(1, 400), randint(1, 100), fill=rgb(randint(0,255), randint(0,255), randint(0,255)))
